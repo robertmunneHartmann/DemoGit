8050f82e7e74cb0a2a550f5675d06553158ddee0
Pol Garcia <Pol.Garcia@hartmann.info>
2023-04-13 15:26:05 +0200

    Corretgit

--- GitBranches/Program.cs
+++ GitBranches/Program.cs
@@ -13,8 +13,10 @@ namespace GitBranches
             Console.WriteLine("Demo Git");
             Console.WriteLine("Estas son las preguntas que vamos a contestar: ");
             Console.WriteLine("1. Cuanto tiempo llevas en Hartmann?");
+            Console.WriteLine("Esta es la rama de Robert");
+            Console.WriteLine("1. 4 meses");
             Console.WriteLine("Esta es la rama de Pol");
-            Console.WriteLine("1 mes");
+            Console.WriteLine("2. 1 mes");
         }
     }
 }
