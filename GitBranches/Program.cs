9b56dd50abcce5e993b44cf723d791e26a648140
Pol Garcia <Pol.Garcia@hartmann.info>
2023-04-13 15:54:31 +0200

    Merge Conflict

--- GitBranches/Program.cs
+++ GitBranches/Program.cs
@@ -14,8 +14,13 @@ namespace GitBranches
             Console.WriteLine("Estas son las preguntas que vamos a contestar: ");
             Console.WriteLine("1. Cuanto tiempo llevas en Hartmann?");
             Console.WriteLine("2. Cual es tu mascota favorita?");
-            Console.WriteLine("Esta es la rama de Robert");
+            Console.WriteLine("Esta es la respuesta de Robert");
             Console.WriteLine("1. 4 meses");
+            Console.WriteLine("Esta es la respuesta de Jordi");
+            Console.WriteLine("1. llevo 4 meses");
+            Console.WriteLine("Esta es la respuesta de Enrico");
+            Console.WriteLine("1. 4 meses");
+            Console.WriteLine("2. Perro");
             Console.WriteLine("Esta es la respuesta de Pol");
             Console.WriteLine("1. 1 mes");
             Console.WriteLine("2. El tigre de Tasmania");
