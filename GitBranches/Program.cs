11ce2deb6c5dbc9b37243d39a6ce117078ec0dce
Pol Garcia <Pol.Garcia@hartmann.info>
2023-04-13 15:46:33 +0200

    Segona resposta

--- GitBranches/Program.cs
+++ GitBranches/Program.cs
@@ -13,10 +13,12 @@ namespace GitBranches
             Console.WriteLine("Demo Git");
             Console.WriteLine("Estas son las preguntas que vamos a contestar: ");
             Console.WriteLine("1. Cuanto tiempo llevas en Hartmann?");
+            Console.WriteLine("2. Cual es tu mascota favorita?");
             Console.WriteLine("Esta es la rama de Robert");
             Console.WriteLine("1. 4 meses");
             Console.WriteLine("Esta es la respuesta de Pol");
             Console.WriteLine("1. 1 mes");
+            Console.WriteLine("2. El tigre de Tasmania");
         }
     }
 }
