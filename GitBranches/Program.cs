a398e35f6705d8670daab25e07e181c2b1b36f25
HG\HG0053129 <Enrico.Martinez@hartmann.info>
2023-04-13 15:22:06 +0200

    Changed to have both answers

--- GitBranches/Program.cs
+++ GitBranches/Program.cs
@@ -12,11 +12,13 @@ namespace GitBranches
         {
             Console.WriteLine("Demo Git");
             Console.WriteLine("Estas son las preguntas que vamos a contestar: ");
-            Console.WriteLine("1. Cunato timepo llevas en Hartmann?");
-            Console.WriteLine("Esta es la rama de Robert");
+            Console.WriteLine("1. Cuanto tiempo llevas en Hartmann?");
+            Console.WriteLine("Esta es la respuesta de Robert");
             Console.WriteLine("1. 4 meses");
-            Console.WriteLine("Esta es la rama de Jordi");
-            Console.WriteLine("1. llevo 4 meses")
+            Console.WriteLine("Esta es la respuesta de Jordi");
+            Console.WriteLine("1. llevo 4 meses");
+            Console.WriteLine("Esta es la respuesta de Enrico");
+            Console.WriteLine("1. 4 meses");
         }
     }
 }
