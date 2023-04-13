6661dc5269429688826fea5e4abdba158eec2c35
Sole Jordi <Jordi.Sole@hartmann.info>
2023-04-13 15:04:54 +0200

    Create the neww branch and answer the questions

--- GitBranches/Program.cs
+++ GitBranches/Program.cs
@@ -12,8 +12,8 @@ namespace GitBranches
         {
             Console.WriteLine("Demo Git");
             Console.WriteLine("Estas son las preguntas que vamos a contestar: ");
-            Console.WriteLine("1. Cunato timepo llevas en Hartmann?");
-            Console.WriteLine("Esta es la rama de Robert");
+            Console.WriteLine("Esta es la rama de Jordi");
+            Console.WriteLine("1. Cunato timepo llevas en Hartmann?")
             Console.WriteLine("1. 4 meses");
         }
     }
