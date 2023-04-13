c9c13711ad2b272c9a05e9110556106e27506963
Pol Garcia <Pol.Garcia@hartmann.info>
2023-04-13 15:06:45 +0200

    Creada la nova branca

--- GitBranches/Program.cs
+++ GitBranches/Program.cs
@@ -12,9 +12,9 @@ namespace GitBranches
         {
             Console.WriteLine("Demo Git");
             Console.WriteLine("Estas son las preguntas que vamos a contestar: ");
-            Console.WriteLine("1. Cunato timepo llevas en Hartmann?");
-            Console.WriteLine("Esta es la rama de Robert");
-            Console.WriteLine("1. 4 meses");
+            Console.WriteLine("1. Cuanto tiempo llevas en Hartmann?");
+            Console.WriteLine("Esta es la rama de Pol");
+            Console.WriteLine("1 mes");
         }
     }
 }
